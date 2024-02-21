6f56953907886cbd4fd8d89cd494df47f5390498
Evan Stohlmann <evans@gen6ventures.com>
2024-02-20 18:11:55 -0700

    initial commit

---
+++ src/main/java/edu/mcc/codeschool/class14/Class14Application.java
@@ -0,0 +1,13 @@
+package edu.mcc.codeschool.class14;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Class14Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Class14Application.class, args);
+	}
+
+}
---
+++ src/test/java/edu/mcc/codeschool/class14/Class14ApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.mcc.codeschool.class14;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Class14ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
